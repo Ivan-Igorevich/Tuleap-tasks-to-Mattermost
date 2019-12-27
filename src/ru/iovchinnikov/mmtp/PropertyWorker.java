5f3890139a2d1cac57b0f51b9a4b2659fc179a6a
Ivan I. Ovchinnikov <ovchinnikov_ii@risde.ru>
2019-12-27 15:14:19 +0300

    added files to current tracker, constants-based search dead-end

--- src/ru/iovchinnikov/mmtp/PropertyWorker.java
+++ src/ru/iovchinnikov/mmtp/PropertyWorker.java
@@ -10,27 +10,29 @@ public class PropertyWorker {
     public static final String PREVIOUS_VERSION = "previous";
 
     public static final String ACTION = "action";
-    public static final String CHANGES_BY = "last_modified_by";     //(String) ((Map) json.get(CHANGES_BY)).get("display_name")
+    public static final String CHANGES_BY = "last_modified_by";
     public static final String CHANGES_TS = "last_modified_date";
-    public static final String LAST_COMMENT = "last_comment";       //(String) ((Map) json.get(LAST_COMMENT)).get("body")
-    public static final String ARTIFACT_VALUES = "values";          //(JSONArray) json.get(ARTIFACT_VALUES);
+    public static final String LAST_COMMENT = "last_comment";
+    public static final String ARTIFACT_VALUES = "values";
     public static final String USER_DISPLAY_NAME = "display_name";
     public static final String USER_NAME = "username";
 
-    // artifact value indexes
-    public static final int TITLE = 0;             // (String) ((Map) values.get(TITLE)).get("value");
+    // TODO a ref-library of id's and fields or label-based search
+    // artifact value indexes (we need to get rid of this, values are hardcoded for this current tracker)
+    public static final int TITLE = 0;
     public static final int TYPE = 1;              // (String) (((Map) ((JSONArray) ((Map) values.get(TYPE)).get("values")).get(0)).get("label"));
     public static final int ASSIGNED_TO = 2;       // (String) (((Map) ((JSONArray) ((Map) values.get(ASSIGNED_TO)).get("values")).get(0)).get("display_name"));
     public static final int STATUS = 3;            // (String) (((Map) ((JSONArray) ((Map) values.get(STATUS)).get("values")).get(0)).get("label"));
     public static final int LINKS = 4;
-    public static final int ARTIFACT_ID = 5;       // (String) ((Map) values.get(ARTIFACT_ID)).get("value");
+    public static final int ARTIFACT_ID = 5;
     public static final int LAST_UPDATE_TS = 6;    // (String) ((Map) values.get(LAST_UPDATE_TS)).get("value");
     public static final int CROSS_REFS = 7;
-    public static final int SUBMITTED_TS = 8;
-    public static final int REMAINING_EFFORT = 9;
-    public static final int LAST_UPDATE_BY = 10;
-    public static final int DETAILS = 11;          // (String) ((Map) values.get(DETAILS)).get("value");
-    public static final int SUBMITTED_BY = 12;
+    public static final int ATTACHMENTS = 8;
+    public static final int SUBMITTED_TS = 9;
+    public static final int REMAINING_EFFORT = 10;
+    public static final int LAST_UPDATE_BY = 11;
+    public static final int DETAILS = 12;
+    public static final int SUBMITTED_BY = 13;
 
     static String getChangesByDisplayName(JSONObject json) {
         return (String) ((Map) json.get(CHANGES_BY)).get(USER_DISPLAY_NAME);
